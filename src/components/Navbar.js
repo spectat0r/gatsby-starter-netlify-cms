2a7386eb5ffb00d59d6fb744eca1f93b29f7d48d
Petro Dydyk <pdydyk@oroinc.com>
2020-06-12 14:28:17 +0300

    test 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,19 +61,13 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                About Us
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Services
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
